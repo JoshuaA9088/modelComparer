e055f700f8fbef109f3510fd736f123a5cfbcf11
joshuaa9088 <joshua.gf.arul@gmail.com>
2018-11-11 18:34:42 -0700

    Removed .pyc and addresses compare gpu utilization issues

--- compare.py
+++ compare.py
@@ -3,16 +3,24 @@ import math
 import argparse
 import os
 import cv2
+import numpy as np
 
+# Import old method of detection
 import cvDetector
-import customModel
 
+# DNN Specific Imports
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+# Parse input image folder
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input_dir", required=True,
                 help="Input dir of XML files and images")
 
 args = vars(ap.parse_args())
 
+# returns list of all files that end with ".x"
 def get_files_by_extension(path, extension):
     xml_list = []
     for filename in os.listdir(path):
@@ -20,73 +28,135 @@ def get_files_by_extension(path, extension):
             xml_list.append(os.path.join(path, filename))
     return xml_list
 
-
 def parser(element):
     # Grab xml node object
 	val = " ".join(t.nodeValue for t in element.childNodes if t.nodeType == t.TEXT_NODE)
 	return val
 
+
+# DNN Necessary Variables
+CWD_PATH = os.getcwd()
+MODEL_NAME = 'graph_144k/'
+PATH_TO_CKPT = '{}frozen_inference_graph.pb'.format(MODEL_NAME)
+PATH_TO_LABELS = 'object-detection.pbtxt'
+NUM_CLASSES = 2
+
+# Load Detection graph
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+# Load label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+IMAGE_SIZE = (12, 8)
+
+def customModel(image_np):
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    (boxes, scores, classes, num_detections) = sess.run(
+    [boxes, scores, classes, num_detections],
+    feed_dict={image_tensor: image_np_expanded})
+
+    height, width, channels = image_np.shape
+
+    boxes = np.squeeze(boxes)
+    classes = np.squeeze(classes)
+    scores = np.squeeze(scores)
+
+    # Chassis Centroid
+    if scores[0] > .5:
+        box = tuple(boxes[0].tolist())
+        yMin = int(box[0] * height)
+        xMin = int(box[1] * width)
+        yMax = int(box[2] * height)
+        xMax = int(box[3] * width)
+
+        xCenter = int((xMax + xMin) / 2)
+        yCenter = int((yMax + yMin) / 2)
+
+        xCenterChassis = int(xCenter)
+        yCenterChassis = int(yCenter)
+
+        return xMin, xMax, yMin, yMax, xCenterChassis, yCenterChassis
+
+# Grab all xmls from input dir
 xmls = get_files_by_extension(args["input_dir"], ".xml")
-centroid_radi = 7
-# jpgs = get_files_by_extension(args["input_dir"], ".jpg")
+# How big should centroid radiuses be
+centroid_radi = 7 
 
-for i in xmls:
-    dom = parse(i)
-    
-    file_list = dom.getElementsByTagName('filename')
-    cat_list = dom.getElementsByTagName('name')
-    
-    width_list = dom.getElementsByTagName("width")
-    height_list = dom.getElementsByTagName("height")
+# Initialize DNN graph once to
+# lower latency
+with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:         
+            for i in xmls:
+                dom = parse(i)
+                
+                file_list = dom.getElementsByTagName('filename')
+                cat_list = dom.getElementsByTagName('name')
+                
+                width_list = dom.getElementsByTagName("width")
+                height_list = dom.getElementsByTagName("height")
 
-    xmin_list = dom.getElementsByTagName('xmin')
-    xmax_list = dom.getElementsByTagName('xmax')
-    ymin_list = dom.getElementsByTagName('ymin')
-    ymax_list = dom.getElementsByTagName('ymax')
-    
+                xmin_list = dom.getElementsByTagName('xmin')
+                xmax_list = dom.getElementsByTagName('xmax')
+                ymin_list = dom.getElementsByTagName('ymin')
+                ymax_list = dom.getElementsByTagName('ymax')
+                
+                for j in range(len(file_list)):
+                    # Parse all the necessary coords and other data
+                    cat = parser(cat_list[j])
+                    filename = parser(file_list[j])
+                    
+                    width = int(parser(width_list[j]))
+                    height = int(parser(height_list[j]))
 
-    for j in range(len(file_list)):
-        # Parse all the necessary coords and other data
-        cat = parser(cat_list[j])
-        filename = parser(file_list[j])
-        
-        width = int(parser(width_list[j]))
-        height = int(parser(height_list[j]))
+                    xmin = int(parser(xmin_list[j]))
+                    xmax = int(parser(xmax_list[j]))
+                    ymin = int(parser(ymin_list[j]))
+                    ymax = int(parser(ymax_list[j]))
 
-        xmin = int(parser(xmin_list[j]))
-        xmax = int(parser(xmax_list[j]))
-        ymin = int(parser(ymin_list[j]))
-        ymax = int(parser(ymax_list[j]))
+                    xCenter = int((xmin + xmax) / 2)
+                    yCenter = int((ymin + ymax) / 2)
 
-        xCenter = int((xmin + xmax) / 2)
-        yCenter = int((ymin + ymax) / 2)
+                    # Read the file based on the xml
+                    img = cv2.imread(args["input_dir"] + filename)
+                    
+                    # Manual Data - Correct
+                    cv2.rectangle(img, (xmin, ymax), (xmax, ymin), (0,255,0), 2)
+                    cv2.circle(img, (xCenter, yCenter), centroid_radi, (0, 255,0), -1)
 
-        # Read the file based on the xml
-        img = cv2.imread(args["input_dir"] + filename)
-        
-        # Manual Data
-        cv2.rectangle(img, (xmin, ymax), (xmax, ymin), (0,255,0), 2)
-        cv2.circle(img, (xCenter, yCenter), centroid_radi, (0, 255,0), -1)
+                    # CV Data - Prediction
+                    contoursChassis, chassisCentroid = cvDetector.show_video(img)
+                    cv2.drawContours(img, contoursChassis, -1, (0,0,255), 2)
+                    if chassisCentroid != None:
+                        cv2.circle(img, chassisCentroid, centroid_radi, (0,0,255), -1)
 
-        # CV Data
-        contoursChassis, chassisCentroid = cvDetector.show_video(img)
-        cv2.drawContours(img, contoursChassis, -1, (0,0,255), 2)
-        if chassisCentroid != None:
-            cv2.circle(img, chassisCentroid, centroid_radi, (0,0,255), -1)
+                    # Custom Model Data - Prediction
+                    xmin, xmax, ymin, ymax, xCenter, yCenter = customModel(img)
+                    cv2.rectangle(img, (xmin, ymax), (xmax, ymin), (255,0,0), 2)
+                    cv2.circle(img, (xCenter, yCenter), centroid_radi, (255, 0,0), -1)
 
-        # Custom Model Dat
-        xmin, xmax, ymin, ymax, xCenter, yCenter = customModel.process_img(img)
-        cv2.rectangle(img, (xmin, ymax), (xmax, ymin), (255,0,0), 2)
-        cv2.circle(img, (xCenter, yCenter), centroid_radi, (255, 0,0), -1)
+                    # Auto resizer if too big
+                    if width > 640 or height > 480:
+                        img = cv2.resize(img, (640, 480))
+                        print("Auto Resized: %s" % filename)
 
-        # Auto resizer if too big
-        if width > 640 or height > 480:
-            img = cv2.resize(img, (640, 480))
-            print("Auto Resized: %s" % filename)
+                    # cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+                    cv2.imshow("Original", img)
 
-        # cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-        cv2.imshow("Original", img)
-
-        c = cv2.waitKey(0)
-        if 'q' == chr(c & 255):
-            exit(0)
+                    # Keypress moves to next image, q exits
+                    c = cv2.waitKey(0)
+                    if 'q' == chr(c & 255):
+                        exit(0)
