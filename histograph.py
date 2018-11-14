d90fd15663a8c04e32d54217e9d4035adbf27a7c
joshuaa9088 <joshua.gf.arul@gmail.com>
2018-11-13 22:41:35 -0700

    added stdev

--- histograph.py
+++ histograph.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import statistics 
 
 cvDetector = []
 dnnDetector = []
@@ -9,26 +10,32 @@ points = 0
 f = open("data.txt", "r")
 
 for i in f.readlines():
-    cvDetector.append(i.split()[1])
-    dnnDetector.append(i.split()[2])
-    points += 1
+    if i.split()[1] != "None" and i.split()[2] != "None":
+        cvDetector.append(float(i.split()[1]))
+        dnnDetector.append(float(i.split()[2]))
+        points += 1
 
-mu, sigma = 100, 15
-# x = mu + sigma*np.random.randn(10000)
-x = dnnDetector
-# print(x)
+cvStd = statistics.stdev(cvDetector)
+print(cvStd)
 
-# the histogram of the data
-n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+f.close()
 
-# add a 'best fit' line
-y = mlab.normpdf( bins, mu, sigma)
-l = plt.plot(bins, y, 'r--', linewidth=1)
+# mu, sigma = 100, 15
+# # x = mu + sigma*np.random.randn(10000)
+# x = dnnDetector
+# # print(x)
 
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
-plt.axis([0, 21, 0, .2])
-plt.grid(True)
+# # the histogram of the data
+# n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
 
-plt.show()
+# # add a 'best fit' line
+# y = mlab.normpdf( bins, mu, sigma)
+# l = plt.plot(bins, y, 'r--', linewidth=1)
+
+# plt.xlabel('Smarts')
+# plt.ylabel('Probability')
+# plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
+# plt.axis([0, 21, 0, .2])
+# plt.grid(True)
+
+# plt.show()
