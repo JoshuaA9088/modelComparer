de4dd99af94d02f443f524676d193275d643e657
JoshuaA9088 <joshua.gf.arul@gmail.com>
2018-11-29 20:44:39 -0700

    Added more plots

--- boxPlot.py
+++ boxPlot.py
@@ -4,6 +4,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import statistics
 
+import T_test
+
 cvDetector = []
 dnnDetector = []
 points = 0
@@ -18,9 +20,42 @@ for i in f.readlines():
         points += 1
 f.close()
 
-fig, ax = plt.subplots()
-ax.set_title('cvDetector v. DnnDetector')
-ax.boxplot(cvDetector, showfliers=False)
-ax.boxplot(dnnDetector, positions=[1,2], showfliers=False)
+# Create a figure instance
+fig = plt.figure(1, figsize=(9, 6))
+
+# Create an axes instance
+ax = fig.add_subplot(111)
+
+# Create the boxplot
+bp = ax.boxplot(cats, patch_artist=True, showfliers=False)
+
+ax.set_xticklabels(catNames)
+
+## change outline color, fill color and linewidth of the boxes
+for box in bp['boxes']:
+    # change outline color
+    box.set(color='#7570b3', linewidth=2)
+    # change fill color
+    box.set(facecolor='#1b9e77')
+
+## change color and linewidth of the whiskers
+for whisker in bp['whiskers']:
+    whisker.set(color='#7570b3', linewidth=2)
+
+## change color and linewidth of the caps
+for cap in bp['caps']:
+    cap.set(color='#7570b3', linewidth=2)
+
+## change color and linewidth of the medians
+for median in bp['medians']:
+    median.set(color='#b2df8a', linewidth=2)
+
+## change the style of fliers and their fill
+for flier in bp['fliers']:
+    flier.set(marker='o', color='#e7298a', alpha=0.5)
+
+plt.title("CV v. DNN Box Plot")
+plt.xlabel("Type of Detector")
+plt.ylabel("Distance from Actual (# of Pixels)")
 
 plt.show()
