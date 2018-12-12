41e922bceccd08b54d89749dabd6074bdca70770
JoshuaA9088 <joshua.gf.arul@gmail.com>
2018-12-12 10:39:35 -0700

    Finished distance and plot scripts

---
+++ plotter/average.py
@@ -0,0 +1,60 @@
+import argparse
+import numpy as np
+import math
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input_dir", required=True,
+                help="Input path of data file")
+
+args = vars(ap.parse_args())
+
+try:
+    f = open(args["input_dir"], "r")
+except FileNotFoundError or FileExistsError:
+    print("Input path not valid")
+    exit(0)
+
+lineCount = 0
+
+cvChassis = []
+dnnChassis = []
+cvBoard = []
+dnnBoard = []
+
+for line in f.readlines():
+    lineCount += 1
+    # If a detection is missing skip
+    # if line.split()[1] == None or line.split()[2] == None: continue
+    try:
+        cvChassis.append(float(line.split()[1]))
+        cvBoard.append(float(line.split()[2]))
+        dnnChassis.append(float(line.split()[3]))
+        dnnBoard.append(float(line.split()[4]))
+    except:
+        continue
+
+cvChassis = sum(cvChassis) / lineCount
+dnnChassis = sum(dnnChassis) / lineCount
+cvBoard = sum(cvBoard) / lineCount
+dnnBoard = sum(dnnBoard) / lineCount
+# dnnDetector = dnnDetector / lineCount
+
+objects = ("cvChassis", "dnnChassis", "cvBoard", "dnnBoard")
+
+y_pos = np.arange(len(objects))
+y = [cvChassis, dnnChassis, cvBoard, dnnBoard]
+
+# print("Average CV: %d" % cvDetector)
+# print("Average DNN: %d" % dnnDetector)
+
+# print("Max CV: %d" % max(cvDetector_list))
+# print("Max DNN: %d" % max(dnnDetector_list))
+
+plt.bar(y_pos, y, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel("Average Distance from Actual (# of Pixels)")
+plt.title("Object Detection Method vs. Average Distance from Actual")
+
+plt.show()
---
+++ plotter/histograph.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import statistics 
+
+cvChassis = []
+cvBoard = []
+dnnChassis = []
+dnnBoard = []
+
+points = 0
+cats = [cvChassis, cvBoard, dnnChassis, dnnBoard]
+catNames = ["cvChassis", "cvBoard", "dnnChassis", "dnnBoard"]
+f = open("distances_board.txt", "r")
+
+for i in f.readlines():
+    if i.split()[1] != "None" and i.split()[2] != "None" and i.split()[3] != "None" and i.split()[4] != "None":
+        cvChassis.append(float(i.split()[1]))
+        cvBoard.append(float(i.split()[2]))
+        dnnChassis.append(float(i.split()[3]))
+        dnnBoard.append(float(i.split()[4]))
+        points += 1
+
+# print(len(cvDetector))
+# print(len(dnnDetector))
+# print(max(cvDetector))
+# print(max(dnnDetector))
+
+# Iterate through both types
+for i in range(len(cats)):
+        mean = np.mean(cats[i])
+        std = np.std(cats[i])
+
+        fig, ax = plt.subplots()
+        fig.tight_layout()
+
+        # Generate x Histogram data
+        n, bins, patches = plt.hist(x=cats[i], bins=50, color='#0504aa',
+                                alpha=0.7, rwidth=0.5)
+        
+        fracs = n / n.max()
+        norm = colors.Normalize(fracs.min(), fracs.max())
+
+        # Y grid creation
+        plt.grid(axis='y', alpha=0.75)
+
+        # Label the axis/title
+        plt.xlabel('Distance from actual (# Pixels)')
+        plt.ylabel('Frequency')
+        plt.title('{} Histogram'.format(catNames[i]))
+        
+        # Appropriately scale the axis
+        maxfreq = n.max()
+        plt.ylim(top=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 20)
+        plt.xlim(0, 20)
+        plt.xticks(np.arange(0, 20, .5), rotation=90)
+        plt.show()
+
+f.close()
+
